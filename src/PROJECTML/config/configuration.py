89ca304f9522c178682603838a9e8007570fa1c4
mahendra867 <mahendramahesh2001@gmail>
2023-12-28 19:36:04 +0530

    data_validation completed successfully

--- src/PROJECTML/config/configuration.py
+++ src/PROJECTML/config/configuration.py
@@ -1,17 +1,15 @@
 from PROJECTML.constants import * # here iam importing everthing which is present in the constants->__init__.py file into inside the data_ingestion.ipynb
 from PROJECTML.utils.common import read_yaml, create_directories # here iam importing the read_yaml, create_directories which are presenting inside the utils,common files into PROJECTML in which the file is data_ingestion.ipynb 
-from PROJECTML.entity.config_entity import DataIngestionConfig 
+from PROJECTML.entity.config_entity import DataIngestionConfig,DataValidationConfig
 
 
 class ConfigurationManager:  # here iam creating class called ConfigurationManager
     def __init__( # inisde this class iam reading all the yaml files which iam calling it from constants->__init__.py file and iam mentioning inside the class varaiable 
         self,
         config_filepath = CONFIG_FILE_PATH,
-        params_filepath = PARAMS_FILE_PATH,
         schema_filepath = SCHEMA_FILE_PATH):
 
         self.config = read_yaml(config_filepath) # and here iam giving read_yaml path here and iam giving the path after that then it will return all the configuration in the variable
-        self.params = read_yaml(params_filepath)
         self.schema = read_yaml(schema_filepath)
 
         create_directories([self.config.artifacts_root])
@@ -31,3 +29,21 @@ class ConfigurationManager:  # here iam creating class called ConfigurationManag
         )
 
         return data_ingestion_config
+    
+
+    # then i will prepare my get_data_validation_config , so inside this iam returning all of the varaibles that i have deifined inside my entity 
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation  # after reading by config iam returning the root_dir,status_file etc one by one
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig( # the above entity code is return type , and the below varaibles are getting return after reading by config varaible 
+            root_dir=config.root_dir, 
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
